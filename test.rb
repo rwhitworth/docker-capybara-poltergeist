0991c615db06e4495f269a0d343d1f9f07492fa7
rwhitworth <me@ryanwhitworth.com>
2016-11-30 21:16:45 -0500

    adding turnip

--- test.rb
+++ test.rb
@@ -1,9 +1,13 @@
 require 'capybara'
 require 'capybara/poltergeist'
 require 'capybara/rspec'
+require 'turnip'
 require 'pry'
+
 Capybara.default_driver = :poltergeist
+
 Capybara.visit 'http://www.google.com'
 puts "Capybara.text: #{Capybara.text}"
 Capybara.save_screenshot('capybara_test_ss.jpg')
+
 binding.pry
