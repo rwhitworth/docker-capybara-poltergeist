34fb4211728a0a36defc0ba75b50e8df52bb6ccb
rwhitworth <me@ryanwhitworth.com>
2016-11-30 20:58:30 -0500

    launch.sh - example mounting of volume

--- test.rb
+++ test.rb
@@ -4,5 +4,6 @@ require 'capybara/rspec'
 require 'pry'
 Capybara.default_driver = :poltergeist
 Capybara.visit 'http://www.google.com'
-puts Capybara.text
+puts "Capybara.text: #{Capybara.text}"
+Capybara.save_screenshot('capybara_test_ss.jpg')
 binding.pry
